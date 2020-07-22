860ab5c0bbfbcd351bdef8c1e90e9248a8218ad1
Shivangi-ch <shivangi.ch.936@gmail.com>
2020-07-22 13:25:45 +0530

    Updates

---
+++ precache-manifest.722101cd12200b93c22d9d138312bde0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc3f574588e261b6f3eb99b2b2a73df",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "aa46e77d252fc1d2be3e",
+    "url": "/my-app/static/css/main.6d52fdd8.chunk.css"
+  },
+  {
+    "revision": "b9156e5b1e91b16c7cc6",
+    "url": "/my-app/static/js/2.a5375784.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.a5375784.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa46e77d252fc1d2be3e",
+    "url": "/my-app/static/js/main.f2c428bd.chunk.js"
+  },
+  {
+    "revision": "f7961c3bc2172f802999",
+    "url": "/my-app/static/js/runtime-main.35cc4f81.js"
+  }
+]);
